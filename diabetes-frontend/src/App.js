3388f5a66062fe0ec73ecc1c2ae09db05adcc641
FallModou <modoufall.8454@gmail.com>
2026-02-12 09:25:33 +0000

    Mise à jour

--- diabetes-frontend/src/App.js
+++ diabetes-frontend/src/App.js
@@ -29,7 +29,7 @@ function App() {
     setLoading(true);
     setPrediction(null);
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("/api/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
